1dbe8627ebb1bd8ef6e59f889a3f85dd04348c12
Mauricio Dias Da Silva <35876@PMB.local>
2025-07-15 17:00:36 -0300

    Commit any pending work before history rewrite

--- energia_solar/settings.py
+++ energia_solar/settings.py
@@ -132,26 +132,26 @@ DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 #     server.quit() if server else None
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'mydb',
-#         'USER': 'user',
-#         'PASSWORD': 'password',
-#         'HOST': 'db',  # mesmo nome do serviço no docker-compose
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         },
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'db',  # mesmo nome do serviço no docker-compose
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
